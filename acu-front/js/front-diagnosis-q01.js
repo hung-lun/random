f0508db04618e4cd8ff894a6382ad5cbbf8ba905
hung-lun <128810619+hung-lun@users.noreply.github.com>
2024-07-05 17:41:46 +0800

    Add files via upload

--- acu-front/js/front-diagnosis-q01.js
+++ acu-front/js/front-diagnosis-q01.js
@@ -1,14 +1,14 @@
-//登出
-var logout1 = document.getElementById("logout");
-logout1.addEventListener("click", logout);
-    function logout() {
-        if (confirm('確認要登出嗎？') == true) {
-            window.location.href = "../acu-front/login.html";
-            localStorage.clear();
-        } else {
-            return false;
-        }
-    }
+// //登出
+// var logout1 = document.getElementById("logout");
+// logout1.addEventListener("click", logout);
+//     function logout() {
+//         if (confirm('確認要登出嗎？') == true) {
+//             window.location.href = "../acu-front/login.html";
+//             localStorage.clear();
+//         } else {
+//             return false;
+//         }
+//     }
 
     //登出---平板
 var logout11 = document.getElementById("logout1");
--- acu-front/js/front-index.js
+++ acu-front/js/front-index.js
@@ -1,18 +1,18 @@
 //登出
-var logout1 = document.getElementById("logout");
+// var logout1 = document.getElementById("logout");
 // var logout2 = document.getElementById("logout1");  //平板RWD登出按鈕
-var logout3 = document.getElementById("logout2");  //手機RWD登出按鈕
-logout1.addEventListener("click", logout);
-// logout2.addEventListener("click", logout);
-logout3.addEventListener("click", logout); 
-    function logout() {
-        if (confirm('確認要登出嗎？') == true) {
-            window.location.href = "../acu-front/login.html";
-            localStorage.clear();
-        } else {
-            return false;
-        }
-    }
+// var logout3 = document.getElementById("logout2");  //手機RWD登出按鈕
+// logout1.addEventListener("click", logout);
+// // logout2.addEventListener("click", logout);
+// logout3.addEventListener("click", logout); 
+//     function logout() {
+//         if (confirm('確認要登出嗎？') == true) {
+//             window.location.href = "../acu-front/login.html";
+//             localStorage.clear();
+//         } else {
+//             return false;
+//         }
+//     }
     
 
     //登出---平板
