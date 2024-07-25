087e49e76a928abf64fe8812cba05c9566aaa4a0
hung-lun <128810619+hung-lun@users.noreply.github.com>
2024-07-25 13:25:14 +0800

    Update front-index.js

--- acu-front/js/front-index.js
+++ acu-front/js/front-index.js
@@ -33,6 +33,7 @@ var logout3 = document.getElementById("logout2");  //手機RWD登出按鈕
                 localStorage.removeItem('fin_score');
 
                 alert("clear successful");
+                location.reload();
             } else {
                 alert("clear fail");
                 return false;
